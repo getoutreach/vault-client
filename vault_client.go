0229e796dc8ce6b4ff8427663bb2d5485033bfdd
Jared Allard <jared.allard@outreach.io>
2021-10-17 21:47:27 -0700

    linteroller slightly happier

--- vault_client.go
+++ vault_client.go
@@ -1,3 +1,5 @@
 // Copyright 2021 Outreach Corporation. All Rights Reserved.
 // Description: Implements a Vault client for use with Vault.
-package vault_client //nolint:revive // Why: we use a - in the name
+//
+// Package vault_client implements a Vault client for use with Vault
+package vault_client //nolint:revive,doculint // Why: we use a - in the name
