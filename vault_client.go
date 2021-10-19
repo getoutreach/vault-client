5ce3b6bfea463cfa44258669325c379e06c9089d
Jared Allard <jared.allard@outreach.io>
2021-10-19 14:38:02 -0700

    make lintroller happy (such garbo)

--- vault_client.go
+++ vault_client.go
@@ -1,5 +1,5 @@
 // Copyright 2021 Outreach Corporation. All Rights Reserved.
 // Description: Implements a Vault client for use with Vault.
-//
+
 // Package vault_client implements a Vault client for use with Vault
-package vault_client //nolint // Why: we use a - in the name
+package vault_client //nolint:doculint,revive // Why: we use a - in the name
