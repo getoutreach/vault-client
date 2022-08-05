50b85ed99229d5768200b95d5f43b22e4eaeee04
Ash Ranjan <49772172+Ashvin-Ranjan@users.noreply.github.com>
2022-08-05 09:12:41 -0700

    feat: Add EnsureLoggedIn function to allow users to log into vault (#46)
    * feat: Add EnsureLoggedIn function to allow users to log into vault
    
    * Copied and modified from devenv because the stork cli will use it
    
    * fix: move EnsureLoggedIn into a different location and package
    
    * chore: add describing file

---
+++ cli/cli.go
@@ -0,0 +1,5 @@
+// Copyright 2022 Outreach Corporation. All Rights Reserved.
+// Description: Implements a way to log in for CLIs.
+
+// Package cli implements a way to log into vault for CLIs
+package cli //nolint:doculint,revive // Why: we use a - in the name
